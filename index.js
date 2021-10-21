74037e198091f0ef4fad531522b50fb4834a50fa
Ismaila Azeez Oluwatobi <azeezthegenius@gmail.com>
2021-10-21 19:42:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ randomnum1= Math.floor((randomnum1 *6)+1 );
 
 document.querySelectorAll("img")[0].setAttribute("src","dice"+randomnum+".png");
 document.querySelectorAll("img")[1].setAttribute("src","dice"+randomnum1+".png");
- alert("Hello,welcome to my Mini Dice Game")
+ alert("Hello,welcome to my Mini Dice Game. This is just a game to check who will throw the better dice.")
 var player1 = prompt("Player 1, fill in your name: ")
 var player2 = prompt("Player 2, fill in your name: ")
 if (randomnum > randomnum1) {
